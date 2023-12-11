c1a56cc4f36d2625222ddef66bd02848761dded1
Jurgis Bridžius <jurgis@bridzius.lt>
2023-12-11 22:36:41 +0200

    Formatting

--- 1/b.mjs
+++ 1/b.mjs
@@ -3,30 +3,48 @@ import { readFile } from "fs/promises";
 const input = await readFile("./input.txt");
 const stringified_input = input.toString();
 
-const words = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+const words = [
+        "one",
+        "two",
+        "three",
+        "four",
+        "five",
+        "six",
+        "seven",
+        "eight",
+        "nine",
+];
 const numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 function findNumber(input, first = true) {
-	let result = 0, i = 1;
-	while (result === 0) {
-		const test = first ? input.substring(0, i) : input.substring(input.length - i, input.length);
-		const word = words.some(w => test.includes(w))
-		const num = numbers.some(n => test.includes(n));
+        let result = 0,
+                i = 1;
+        while (result === 0) {
+                const test = first
+                        ? input.substring(0, i)
+                        : input.substring(input.length - i, input.length);
+                const word = words.some((w) => test.includes(w));
+                const num = numbers.some((n) => test.includes(n));
 
-		const checker = word ? words : num ? numbers : [];
-		for (let j = 0; j < checker.length; j++) {
-			result = test.indexOf(checker[j]) !== -1 ? j+1 : result;
-		}
-		i++;
-	}
-	return result;
+                const checker = word ? words : num ? numbers : [];
+                for (let j = 0; j < checker.length; j++) {
+                        result =
+                                test.indexOf(checker[j]) !== -1
+                                        ? j + 1
+                                        : result;
+                }
+                i++;
+        }
+        return result;
 }
 
-
-const result = stringified_input.split("\n").filter(i => i).reduce((acc, curr) => {
-	const first = findNumber(curr);
-	const last = findNumber(curr, false);
-	return acc + Number(`${first}${last}`);
-}, 0);
+const result = stringified_input
+        .split("\n")
+        .filter((i) => i)
+        .reduce((acc, curr) => {
+                const first = findNumber(curr);
+                const last = findNumber(curr, false);
+                return acc + Number(`${first}${last}`);
+        }, 0);
 
 console.log(result);
--- 2/b.mjs
+++ 2/b.mjs
@@ -5,23 +5,45 @@ const colors = ["red", "green", "blue"];
 
 const input = (await readFile("./input.txt")).toString("utf-8");
 
-const result = input.split('\n').filter(l => l).map(row => {
-	const values = row.substring(row.indexOf(":")+1, row.length)
-		.replaceAll(" ", "").split(";");
-	return values.map(v => {
-		let map = {red: 0, green: 0, blue: 0};
-		const cubes = v.split(",");
-		for(let i = 0; i < cubes.length; i++) {
-			const color = colors.find(c => cubes[i].indexOf(c) !== -1);
-			map[color] = Number(cubes[i].replace(color, ""));
-		}
-		return [map.red, map.green, map.blue];
-	}).reduce((r, g) => {
-		let res = [];
-		for (let i = 0; i < r.length; i++) {
-			res.push(g[i]>r[i] ? g[i] : r[i]);
-		}
-		return res;
-	}, [0, 0, 0]);
-}).map(cubes => cubes.reduce((l, c) => l*c, 1)).reduce((res, pow) => res + pow, 0);
+const result = input
+        .split("\n")
+        .filter((l) => l)
+        .map((row) => {
+                const values = row
+                        .substring(row.indexOf(":") + 1, row.length)
+                        .replaceAll(" ", "")
+                        .split(";");
+                return values
+                        .map((v) => {
+                                let map = { red: 0, green: 0, blue: 0 };
+                                const cubes = v.split(",");
+                                for (let i = 0; i < cubes.length; i++) {
+                                        const color = colors.find(
+                                                (c) =>
+                                                        cubes[i].indexOf(c) !==
+                                                        -1,
+                                        );
+                                        map[color] = Number(
+                                                cubes[i].replace(color, ""),
+                                        );
+                                }
+                                return [map.red, map.green, map.blue];
+                        })
+                        .reduce(
+                                (r, g) => {
+                                        let res = [];
+                                        for (let i = 0; i < r.length; i++) {
+                                                res.push(
+                                                        g[i] > r[i]
+                                                                ? g[i]
+                                                                : r[i],
+                                                );
+                                        }
+                                        return res;
+                                },
+                                [0, 0, 0],
+                        );
+        })
+        .map((cubes) => cubes.reduce((l, c) => l * c, 1))
+        .reduce((res, pow) => res + pow, 0);
 console.log(result);
